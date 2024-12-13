0fd2e850e2d8a6ea42dfec5c3b27dfbbf3335bb4
Arthur Le Floch <arthur.le_floch@bordeaux-inp.fr>
2024-12-13 13:25:28 +0100

    Fix error in post pusher

--- airflow/dags/post_pusher.py
+++ airflow/dags/post_pusher.py
@@ -5,13 +5,12 @@ from airflow.decorators import task
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
 # Supposed to run every minute
-with DAG(dag_id="post_pusher", start_date=datetime(2022, 1, 1), schedule="* * * * *", catchup=False) as dag:
+with DAG(dag_id="post_pusher", start_date=datetime(2022, 1, 1), schedule="* * * * *", catchup=False, is_paused_upon_creation=False) as dag:
     run_script = KubernetesPodOperator(
         task_id='run_post_pusher',
         name='post-pusher',
         namespace='projet',
         image='2024_kubernetes_post_pusher',
         image_pull_policy='Never',
-        is_delete_operator_pod=True,
-        is_paused_upon_creation=False
+        is_delete_operator_pod=True
     )
